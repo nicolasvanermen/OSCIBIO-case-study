b16507da1451dc9681c309606851342c101fc8c7
nicolasvanermen <nicolas.vanermen@inbo.be>
2024-02-05 09:27:57 +0100

    Update Oscibio case study script.R

--- Oscibio case study script.R
+++ Oscibio case study script.R
@@ -1,20 +1,3 @@
-# To include your GBIF credentials in this R session
-# 
-# library(usethis)
-# usethis::edit_r_environ()
-# Edit your .Renviron to look like this:
-#   
-# GBIF_USER="nicolasvanermen"
-# GBIF_PWD="************"
-# GBIF_EMAIL="nicolas.vanermen@inbo.be"
-# 
-# Also see: https://docs.ropensci.org/rgbif/articles/gbif_credentials.html
-
-# To get a boundary map of Flanders you need "rnaturalearthhires":
-# 
-# install.packages("rnaturalearthhires", repos = "https://ropensci.r-universe.dev", type = "source")
-
-library(ggplot2)
 library(rgbif)
 library(rnaturalearth)
 library(sf)
@@ -40,7 +23,7 @@ Vespa_velutina_FL <- Download_GBIF_occurrences(selected_species, selected_region
 #Tests:
 #Test the result graphically:
 Flanders <- ne_states(country = "Belgium", geounit = "Flemish Region", returnclass = "sf")
-mapview(Vespa_velutina_FL, cex=0.3, col.regions = "black", layer.name='Vespa velutina (GBIF occurrences)') + mapview(Flanders, col.regions = "darkgreen", alpha.regions = 0.1)
+mapview(Vespa_velutina_FL, cex = 0.3, col.regions = "black", layer.name = 'Vespa velutina (GBIF occurrences)') + mapview(Flanders, col.regions = "darkgreen", alpha.regions = 0.1)
 
 #Test the selected species:
 unique(Vespa_velutina_FL$species) == species_name
@@ -51,18 +34,18 @@ sum(ymd(paste(Vespa_velutina_FL$year,Vespa_velutina_FL$month,Vespa_velutina_FL$d
   nrow(Vespa_velutina_FL)
 
 #Test the geographical range:
-# Define a bounding box of the selected area
-# source:https://geometadatalabs.eu/index.php?title=Land_cover_Belgium_(Flanders)_1m_resolution_2012
+#Define a bounding box of the selected area
+#source:https://geometadatalabs.eu/index.php?title=Land_cover_Belgium_(Flanders)_1m_resolution_2012
 North <- 51.49590
 East <- 5.92000
 South <- 50.67480
 West <- 2.56035
 #perform test with a 1% relaxation of the geographical limits:
 coord <- as.data.frame(st_coordinates(Vespa_velutina_FL))
-sum(coord$X < East+0.1 & 
-      coord$X > West-0.1 & 
-      coord$Y > South-0.1 & 
-      coord$Y < North+0.1) == nrow(Vespa_velutina_FL)
+sum(coord$X < East + 0.1 & 
+      coord$X > West - 0.1 & 
+      coord$Y > South - 0.1 & 
+      coord$Y < North + 0.1) == nrow(Vespa_velutina_FL)
 
 
 #Define the input variables for the graph generator
